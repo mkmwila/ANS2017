f6f3faf135604505acd66b50db49b73563c08093
Perseverance <pmbewe@csir.co.za>
2017-11-23 15:09:24 +0200

    added Global local pose to the waterfall model - gets angle in real time from Global Pose

--- 3. World Model Vector Knowledge Store (ID 61)/World_Molde_Update/server.js
+++ 3. World Model Vector Knowledge Store (ID 61)/World_Molde_Update/server.js
@@ -1,6 +1,4 @@
 
-
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -9,7 +7,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mapInterfaceModel = require('./models/mongoSchema')
 var mongoose  = require('mongoose');
-mongoose.connect('mongodb://localhost/atnMessageInterface');
 var mapInterfaceUtil = mapInterfaceModel.mapInterfaceModel;
 var synchronisationUtil = require('./models/worldModel.js');
 var app = express();
@@ -21,15 +18,22 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// connect to the mongodb database 
+mongoose.connect('mongodb://localhost/atnMessageInterface');
+
 
 /*
  routes - dedicated for the localiser and traversability grid 
 */
 var positionObject ={};
+var angleObject ={};
+var yawObject = {};
 // localPose
 app.post('/worldModel/localPose/position',function(req,res){
     var positionData = req.body;
-    positionData = {'x':parseFloat(positionData.x),'y':parseFloat(positionData.y),'r':parseFloat(positionData.r)};
+    var yaw = getYawAngle();
+    console.log('yaw angle about to be sent', yaw);
+    positionData = {'x':parseFloat(positionData.x),'y':parseFloat(positionData.y),'yaw': parseInt(getYawAngle().yaw)};
     console.log('postion data',positionData);
     setPositionPose(positionData);
     res.json({'message':positionData});
@@ -39,7 +43,6 @@ app.post('/worldModel/localPose/position',function(req,res){
 app.get('/worldModel/localiser/position',function(req,res){
     // passing the location data to the localiser
     var positionData = getPositionPose();
-    
     console.log('gettting location', positionData);
     res.json(positionData);
 });
@@ -47,6 +50,25 @@ app.get('/worldModel/localiser/position',function(req,res){
 app.post('/worldModel/position',synchronisationUtil.getVihicePosition) // gets position 
 app.post('/worldModel/traversability',synchronisationUtil.getVehicleClass) // gets class for traversability 
 app.post('/worldModel/obstacles',synchronisationUtil.getDynamicObstacleDetection) // route for dynamic obstabcle detection
+
+/*
+ * getting the yho angle from the Global Pose 
+*/
+
+app.post('/worldModel/GPose/angle',function(req,res){
+    var yaw = req.body;
+    console.log('yaw angle', yaw);
+    setYawAngle(yaw);
+    console.log('Global Pose Yaw angle', yaw);
+    res.json({"message": 'angle recieved'});
+});
+// parse yaw angle to the localiser
+app.get('/worldModel/globalpose/yaw',function(req,res){
+    // send yaw angle to the localiser
+    var yaw = getYawAngle();
+    console.log('sending Yaw Angle ..', yaw);
+    res.json(yaw);
+})
 
 /*
   Hepler Region
@@ -60,7 +82,15 @@ function getPositionPose(){
 return positionObject;
 }
 
-app.listen(3000,function(){
+function setYawAngle(yaw){
+    yawObject = yaw;
+    return;
+}
 
-console.log('world model lsitening at %s', '3000');
+function getYawAngle(){
+    return yawObject;
+}
+
+app.listen(3000,function(){
+    console.log('world model lsitening at %s', '3000');
 });
