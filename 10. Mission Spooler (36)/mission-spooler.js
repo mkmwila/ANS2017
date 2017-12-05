1ff9b776a514b1da6705ed233229fdd85063a573
Martin Mwila <mmwila@users.noreply.github.com>
2017-12-05 14:33:51 +0200

    Add files via upload

--- 10. Mission Spooler (36)/mission-spooler.js
+++ 10. Mission Spooler (36)/mission-spooler.js
@@ -14,11 +14,15 @@
     ==========================================================================*/
 const moment = require('moment');
 const io = require('socket.io-client');
-const socket = io.connect('http://C6-MiFi:3000', { reconnect: true });  // replaced the ip address 
-   var me = {
+const socket = io.connect('http://localhost:3000', { reconnect: true });  // replaced the ip address 
+var me = {
     id: 36,
     name: 'Mission Spooler'
 };
+var subSystemCommander = {
+    id:32,
+    name: 'Sub-System Commander'
+}; 
 var systemTree = [me];
 
 console.log('\n\n***** Mission Spooler is Running! *****\n')
@@ -56,5 +60,56 @@ socket.on('connect', () => { // begining of the connection estalished block of c
     socket.on('disconnect', () => {
         console.log('Connection to the G-Bat network has been terminated!');
     });
-   
+
 });// end of the connection estalished block of code
+var ack = {
+    command: '0403h'
+};
+/* Handling the messages */
+socket.on('0E00h', (nodeInfo, acknowledge) => {
+    console.log(`\n => ${nodeInfo.sender.name} is requesting to start the mission ${nodeInfo.data.name}`);
+    
+    socket.emit('4400h', nodeInfo, (ack) => {
+        console.log(JSON.stringify(ack, null, 4));
+    });
+    nodeInfo.recipient= subSystemCommander,
+    socket.emit('0E00h', nodeInfo, (ack) => {
+      console.log(JSON.stringify(ack, null, 4));
+  });
+});
+
+socket.on('0E02h', (nodeInfo, acknowledge) => {
+    console.log(`\n => ${nodeInfo.sender.name} is requesting to stop the mission ${nodeInfo.data.name}`);
+ 
+    socket.emit('4400h', nodeInfo, (ack) => {
+        console.log(JSON.stringify(ack, null, 4));
+    });
+    nodeInfo.recipient= subSystemCommander,
+    socket.emit('0E02h', nodeInfo, (ack) => {
+      console.log(JSON.stringify(ack, null, 4));
+  });
+});
+
+socket.on('0E03h', (nodeInfo, acknowledge) => {
+    console.log(`\n => ${nodeInfo.sender.name} is requesting to pause the mission ${nodeInfo.data.name}`);
+ 
+    socket.emit('4400h', nodeInfo, (ack) => {
+        console.log(JSON.stringify(ack, null, 4));
+    });
+    nodeInfo.recipient= subSystemCommander,
+    socket.emit('0E03h', nodeInfo, (ack) => {
+      console.log(JSON.stringify(ack, null, 4));
+  });
+});
+
+socket.on('0E04h', (nodeInfo, acknowledge) => {
+    console.log(`\n => ${nodeInfo.sender.name} is requesting to resume the mission ${nodeInfo.data.name}`);
+ 
+    socket.emit('4400h', nodeInfo, (ack) => {
+        console.log(JSON.stringify(ack, null, 4));
+    });
+    nodeInfo.recipient= subSystemCommander,
+    socket.emit('0E04h', nodeInfo, (ack) => {
+      console.log(JSON.stringify(ack, null, 4));
+  });
+});
